fdb8c203065fd7d4747cf51d670606cd26ecbbf8
IkerMiguel <ikmiguel03@gmail.com>
2025-05-18 16:30:36 -0500

    Destroy Comuna

--- routes/api.php
+++ routes/api.php
@@ -10,5 +10,6 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/comunas', [ComunaController::class, 'store'])->name('comunas.store');
 Route::get('/comunas', [ComunaController::class, 'index'])->name('comunas');
+Route::delete('/comunas/{comuna}', [ComunaController::class, 'destroy'])->name('comunas.destroy');
 Route::get('/comunas/{comuna}', [ComunaController::class, 'show'])->name('comunas.show');
 Route::put('/comunas/{comuna}', [ComunaController::class, 'update'])->name('comunas.update');
